d003902595f27c24e026c93399bf41132af817cb
ruhamabek <ruhamauzubo@gmail.com>
2025-02-11 10:17:10 +0300

    feat: add user management functionality with CRUD operations and health check endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,6 +14,9 @@ app.use(cors());
 
 app.use(express.json());
 
+app.get("/health", async (req: Request, res: Response) => {
+  res.send({ message: "health ok" });
+});
 app.use("/api/my/user", myUserRoute);
 
 app.listen(7000, () => {
